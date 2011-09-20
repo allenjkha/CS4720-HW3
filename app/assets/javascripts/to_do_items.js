45581056c0dd3740425d46f695cdd11ab205638b
Allen J. Kha <allen.kha@gmail.com>
2011-09-20 02:24:15 -0400

    fixing JS

--- app/assets/javascripts/to_do_items.js
+++ app/assets/javascripts/to_do_items.js
@@ -1,9 +1,4 @@
 $(document).ready(function(){
-  $("ul li").mouseup(function(){
-    $(this).children(".item").removeClass("clickstate");
-    $(this).children(".sub").removeClass("visible");
-    }).mousedown(function(){
-      $(this).children(".item").addClass("clickstate");
-      $(this).children(".sub").addClass("visible");
-     });
+  $("ul li").mouseup(function(){$(this).children(".item").removeClass("clickstate");}).mousedown(function(){$(this).children(".item").addClass("clickstate");});
+  $("ul li").mouseout(function(){$(this).children("div").children("a.sub").removeClass("visible");}).mouseover(function(){$(this).children("div").children("a.sub").addClass("visible");});
 });
