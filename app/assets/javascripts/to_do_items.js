bad76a71440b0593fcda96428be5b50097992bf5
Allen J. Kha <allen.kha@gmail.com>
2011-09-21 23:00:16 -0400

    JS effects

--- app/assets/javascripts/to_do_items.js
+++ app/assets/javascripts/to_do_items.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
   $("ul li").mouseup(function(){$(this).children(".item").removeClass("clickstate");}).mousedown(function(){$(this).children(".item").addClass("clickstate");});
   $("ul li").mouseout(function(){$(this).children("div").children("a.sub").removeClass("visible");}).mouseover(function(){$(this).children("div").children("a.sub").addClass("visible");});
+  $("#add").click(function(){
+  	if ($(this).hasClass("top")) {
+  		$(this).animate({"top": "-=100px"}, "slow").removeClass("top").addClass("bottom");
+  	}
+  	else if ($(this).hasClass("bottom")) {
+  		$(this).animate({"top": "+=100px"}, "slow").removeClass("bottom").addClass("top");
+  	} else {}
+  });
 });
